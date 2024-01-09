ca1ea6687bb7b971e16bfbb1622b602907cf4e54
Destiny Oseh <142323138+TheDarkPhoenix15@users.noreply.github.com>
2024-01-09 21:43:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Results Summary</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="./results-summary-component-main/assets/images/favicon-32x32.png">
+    <link rel="stylesheet" href="styling.css">
+</head>
+<body>
+    <main>
+        <section>
+
+            <article id="results">
+                <div id="results_main">
+                    <h2>Your Result</h2>
+                    <div id="spam_cont">
+                        <div id="spam">
+                            <h1>72</h1><p id="of">of 100</p>
+                        </div>
+                    </div>
+                    <h2>Great</h2>
+                    <p id="youscoredhig">You scored higher than 65% of people who have taken these tests.</p>
+                </div>
+            </article>
+
+            <article id="summaryCont">
+                <div id="summary">
+                    <h2 id="sum">Summary</h2>
+                    <ul id="values">
+                        <li id="reaction"><span id="r">Reaction</span> <div class=fractions><b>80</b> / 100</div></li>
+                        <li id="memory"><span id='m'>Memory</span> <div class=fractions><b>92</b> / 100</div></li>
+                        <li id="verbal"><span id='ve'>Verbal</span> <div class=fractions2><b>61</b> / 100</div></li>
+                        <li id="visual"><span id="vi">Visual</span> <div class=fractions2><b>72</b> / 100</div></li>
+                    </ul>
+
+                    <a href="continue.php"><button>Continue</button></a>
+                </div>
+            </article>
+
+        </section>
+    </main>
+</body>
+</html>
